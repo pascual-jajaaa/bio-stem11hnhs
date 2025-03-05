c83e66e99089a6a2858c9e9f1a970cf940c19b72
pascual-jajaaa <jardysaurusss@gmail.com>
2025-03-05 21:53:58 +0800

    tryyy

--- folder/quizz/index.js
+++ folder/quizz/index.js
@@ -1,69 +1,27 @@
 const survey = new Survey.Model(json);
 survey.applyTheme(themeJson);
 survey.onComplete.add((sender, options) => {
+    const surveyData = sender.data;
+
+    // Append userData to user.json
+    $.ajax({
+        url: './user.json',
+        type: 'GET',
+        dataType: 'json',
+        success: function(data) {
+            data.push(surveyData);
+            $.ajax({
+                url: './user.json',
+                type: 'POST',
+                contentType: 'application/json',
+                data: JSON.stringify(data),
+                success: function() {
+                    console.log('User data saved successfully.');
+                }
+            });
+        }
+    });
+
     console.log(JSON.stringify(sender.data, null, 3));
 });
 $("#surveyElement").Survey({ model: survey });
-
-
-import fs from "fs";
-// reading a JSON file asynchronously
-fs.readFile("user.json", (error, data) => {
-    // if the reading process failed,
-    // throwing the error
-    if (error) {
-      // logging the error
-      console.error(error);
-  
-      throw err;
-    }
-  
-    // parsing the JSON object
-    // to convert it to a JavaScript object
-    const user = JSON.parse(data);
-  
-    // printing the JavaScript object
-    // retrieved from the JSON file
-    console.log(user);
-  });
-
-
-
-
-
-
-
-const surveyResults = [{
-    "satisfaction-score": 5,
-    "nps-score": 10
-}, {
-    "satisfaction-score": 5,
-    "nps-score": 9
-}, {
-    "satisfaction-score": 3,
-    "nps-score": 6
-}, {
-    "satisfaction-score": 3,
-    "nps-score": 6
-}, {
-    "satisfaction-score": 2,
-    "nps-score": 3
-}];
-
-const vizPanelOptions = {
-    allowHideQuestions: false
-}
-
-Survey.slk(
-    "OWQyYzczMzktMWI5NS00OTMxLWE0YjgtMDRhMGY0NzZhOWZhJmRvbWFpbnM6c3VydmV5anMuaW8sc3VydmV5anN0ZXN0LmF6dXJld2Vic2l0ZXMubmV0OzE9MjIyNC0wMi0wOCwyPTIyMjQtMDItMDgsND0yMjI0LTAyLTA4"
-);
-
-const vizPanel = new SurveyAnalytics.VisualizationPanel(
-    survey.getAllQuestions(),
-    surveyResults,
-    vizPanelOptions
-);
-
-document.addEventListener("DOMContentLoaded", function() {
-    vizPanel.render(document.getElementById("surveyVizPanel"));
-});
