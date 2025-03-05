2238720a56e51a34a7d8517f15edab0e88bcf00e
pascual-jajaaa <jardysaurusss@gmail.com>
2025-03-05 20:55:49 +0800

    fix quiz js

--- folder/quizz/index.js
+++ folder/quizz/index.js
@@ -5,6 +5,27 @@ survey.onComplete.add((sender, options) => {
 });
 $("#surveyElement").Survey({ model: survey });
 
+const surveyResults = [{
+    "satisfaction-score": 5,
+    "nps-score": 10
+}, {
+    "satisfaction-score": 5,
+    "nps-score": 9
+}, {
+    "satisfaction-score": 3,
+    "nps-score": 6
+}, {
+    "satisfaction-score": 3,
+    "nps-score": 6
+}, {
+    "satisfaction-score": 2,
+    "nps-score": 3
+}];
+
+const vizPanelOptions = {
+    allowHideQuestions: false
+}
+
 Survey.slk(
     "OWQyYzczMzktMWI5NS00OTMxLWE0YjgtMDRhMGY0NzZhOWZhJmRvbWFpbnM6c3VydmV5anMuaW8sc3VydmV5anN0ZXN0LmF6dXJld2Vic2l0ZXMubmV0OzE9MjIyNC0wMi0wOCwyPTIyMjQtMDItMDgsND0yMjI0LTAyLTA4"
 );
